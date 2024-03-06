e43a25e5fb50a4d7b984cfebe1ff477c643e0238
Charitha Weerasekara <charitha.weerasekara@globalwavenet.com>
2024-03-06 11:30:08 +0530

    button updates

--- ui-test/projects/comda-ui/src/lib/button/button.component.ts
+++ ui-test/projects/comda-ui/src/lib/button/button.component.ts
@@ -8,12 +8,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ButtonComponent {
   // Input properties
-  @Input() placeholder: string = ''; // The placeholder text of the button
-  @Input() buttonText: string = 'Button'; // The button text
+  @Input() buttonText: string = ''; // The button text
   @Input() disabled: boolean = false; // The disabled state of the button
   @Input() icon: string = ''; // The icon of the button
   @Input() iconPosition: 'left' | 'right' = 'left'; // The position of the icon
   @Input() size: 'small' | 'medium' | 'large' = 'medium'; // The size of the button
+  @Input() btnColor: string = '#007BFF'; // The color of the button
+  @Input() txtColor: string = 'white'; // The color of the button text
 
   // Output event
   @Output() click = new EventEmitter<void>(); // The click event of the button
@@ -24,6 +25,19 @@ export class ButtonComponent {
   onClick() {
     if (!this.disabled) {
       this.click.emit();
+    }
+  }
+
+  getFontSize() {
+    switch (this.size) {
+      case 'small':
+        return 12;
+      case 'medium':
+        return 16;
+      case 'large':
+        return 20;
+      default:
+        return 16;
     }
   }
 }
